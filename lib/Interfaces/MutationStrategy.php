6d83dfb08022779142c614b86d8b94b50a094dde
Alex Standiford <alex@standiford.us>
2023-11-08 06:38:24 -0600

    corrected strategy return type

--- lib/Interfaces/MutationStrategy.php
+++ lib/Interfaces/MutationStrategy.php
@@ -5,8 +5,8 @@ interface MutationStrategy
 {
     /**
      * @param callable():MutatorHandler $mutatorGetter
-     * @param callable $action
+     * @param string $action
      * @return void
      */
-    public function attach(callable $mutatorGetter, callable $action): void;
+    public function attach(callable $mutatorGetter, string $action): void;
 }
